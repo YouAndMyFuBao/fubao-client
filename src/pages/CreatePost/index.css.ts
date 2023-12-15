e12f3277d95c0563c23f3558d5853b57b30bcc40
eunchokang <wkdb0425@gmail.com>
2023-12-15 20:28:33 +0900

    :sparkles: 만들기 화면 기본 기능 구현

--- src/pages/CreatePost/index.css.ts
+++ src/pages/CreatePost/index.css.ts
@@ -2,32 +2,49 @@ import { style } from '@vanilla-extract/css';
 
 export const createPostStyles = {
   postWrapper: style({
-    boxShadow: '0 0 0 5px red inset',
+    // boxShadow: '0 0 0 5px red inset',
+    position: 'relative',
   }),
 
-  postImage: style({
-    backgroundColor: 'gray',
-    width: '300px',
-    height: '300px',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-  }),
+  image: {
+    nonePostImage: style({
+      backgroundColor: 'gray',
+      width: '300px',
+      height: '300px',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+    }),
+    nonPostImageInput: style({
+      display: 'none',
+    }),
+    selectedPostImage: style({
+      display: 'block',
+      width: '300px',
+      height: '300px',
+      objectFit: 'cover',
+    }),
+    deleteButton: style({
+      position: 'absolute',
+      top: '10px',
+      left: '10px',
+    }),
+  },
 
-  postText: style({
-    backgroundColor: 'indigo',
-    height: '300px',
-  }),
+  text: {
+    postText: style({
+      backgroundColor: 'indigo',
+      height: '300px',
+    }),
 
-  postTextInput: style({
-    width: '90%',
-    height: '70%',
-    margin: '10px 10px',
-  }),
+    postTextInput: style({
+      width: '90%',
+      height: '70%',
+      margin: '10px 10px',
+    }),
+  },
 
   submitButton: style({
-    boxShadow: '0 0 0 5px green inset',
-    height: '100px',
     fontSize: '20px',
   }),
 };
--- src/pages/CreatePost/index.tsx
+++ src/pages/CreatePost/index.tsx
@@ -1,17 +1,83 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { createPostStyles } from './index.css';
 
 export default function CreatePost() {
+  const [postImage, setPostImage] = useState<File | null>(null);
+  const [postText, setPostText] = useState<string>('');
+
+  const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files) {
+      const newImage = e.target.files[0];
+      console.log(newImage);
+      setPostImage(newImage);
+    }
+  };
+
+  const handleDeleteButtonClick = () => {
+    const responseConfirm = window.confirm('정말로 삭제하시겠습니까?');
+    if (responseConfirm) {
+      setPostImage(null);
+    }
+  };
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(e.target.value);
+    setPostText(e.target.value);
+  };
+
+  const handleSubmitPost = () => {
+    if (postImage && postText.length > 0) {
+      console.log('Image', postImage);
+      console.log('Text', postText);
+    } else {
+      window.alert('이미지 혹은 텍스트가 누락되었는지 확인하세요.');
+    }
+  };
+
   return (
     <>
       <div className="wrapper">
-        <div className={createPostStyles.postWrapper}>
-          <button className={createPostStyles.postImage}>사진을 업로드해주세요.</button>
-          <div className={createPostStyles.postText}>
-            <input type="text" className={createPostStyles.postTextInput} />
+        {postImage ? (
+          <div className={createPostStyles.postWrapper}>
+            <img
+              className={createPostStyles.image.selectedPostImage}
+              src={URL.createObjectURL(postImage)}
+              alt="postImageSelected"
+            />
+            <button
+              className={createPostStyles.image.deleteButton}
+              type="button"
+              onClick={handleDeleteButtonClick}
+            >
+              삭제X
+            </button>
           </div>
+        ) : (
+          <div className={createPostStyles.postWrapper}>
+            <label htmlFor="file" className={createPostStyles.image.nonePostImage}>
+              <div className="btn-upload">사진을 업로드해주세요.</div>
+            </label>
+            <input
+              type="file"
+              id="file"
+              accept="image/*"
+              required
+              className={createPostStyles.image.nonPostImageInput}
+              onChange={handleImageChange}
+            />
+          </div>
+        )}
+        <div className={createPostStyles.text.postText}>
+          <input
+            type="text"
+            className={createPostStyles.text.postTextInput}
+            value={postText}
+            onChange={handleInputChange}
+          />
         </div>
-        <button className={createPostStyles.submitButton}>제출하기</button>
+        <button className={createPostStyles.submitButton} onClick={handleSubmitPost}>
+          제출하기
+        </button>
       </div>
     </>
   );
