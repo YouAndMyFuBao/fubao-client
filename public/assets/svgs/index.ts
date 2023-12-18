7b449ec56b6c5398acd94ed565ea768b7bf5f034
anonymousrecords <a.Records2022@gmail.com>
2023-12-18 16:43:32 +0900

    :lipstick: svg 파일 추가

---
+++ public/assets/svgs/index.ts
@@ -0,0 +1,5 @@
+import IconArrowBack from './IconArrowBack.svg';
+import IconNavigationButton from './IconNavigationButton.svg';
+import IconHamburger from './IconHamburger.svg';
+
+export { IconArrowBack, IconNavigationButton, IconHamburger };
