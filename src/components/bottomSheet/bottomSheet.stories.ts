bacdabe57c849e3a50ec47b9e874d5df2e0d52ac
euncho.kang <wkdb0425@gmail.com>
2023-12-25 23:05:11 +0900

    :sparkles: bottomSheet에 대한 storybook 파일 작성

--- src/components/bottomSheet/bottomSheet.stories.ts
+++ src/components/bottomSheet/bottomSheet.stories.ts
@@ -12,14 +12,43 @@ export default meta;
 
 type Story = StoryObj<typeof BottomSheet>;
 
-export const DeleteModal: Story = {
+export const KakaoLoginBottomSheet: Story = {
   args: {
-    children: '정말 삭제하시겠습니까?',
+    variants: 'login',
   },
 };
 
-export const EditModal: Story = {
+export const SignOutBottomSheet: Story = {
   args: {
-    children: '수정하시겠습니까?',
+    variants: 'common',
+    children: `디어바오와 작별합니다ㅜ 
+    다음에 다시 만나요 :)`,
+    primaryButtonChildren: '탈퇴하기',
+  },
+};
+
+export const LogOutBottomSheet: Story = {
+  args: {
+    variants: 'common',
+    children: `로그아웃 하시겠어요?
+    다음에 다시 만나요 :)`,
+    primaryButtonChildren: '로그아웃',
+  },
+};
+
+export const DeleteLetterBottomSheet: Story = {
+  args: {
+    variants: 'common',
+    children: `현재 작성 중인 내용이 저장되지 않았어요.
+    삭제하시겠어요?`,
+    primaryButtonChildren: '삭제하기',
+  },
+};
+
+export const DeleteMyLetter: Story = {
+  args: {
+    variants: 'common',
+    children: '편지를 삭제하시겠어요?',
+    primaryButtonChildren: '삭제하기',
   },
 };
