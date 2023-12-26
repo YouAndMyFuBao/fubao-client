cb9a224fec0c2bbda131c1b796f89517b710ff96
anonymousrecords <a.Records2022@gmail.com>
2023-12-26 23:47:26 +0900

    :zap: font 설정

---
+++ public/fonts/fonts.ts
@@ -0,0 +1,9 @@
+import localFont from 'next/font/local';
+
+export const pretendardFont = localFont({
+  src: './PretendardVariable.woff2',
+});
+
+export const letterFont = localFont({
+  src: './UhBeeKang-Ja.woff',
+});
