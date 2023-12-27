755961bc3ebef5eae8ec7c0ae150a1d80f093297
euncho.kang <107094445+cho7778@users.noreply.github.com>
2023-12-27 23:05:54 +0900

    [FEAT] Snackbar 공통 컴포넌트 구현 (#31)
    * :lipstick: snackbar 컴포넌트 구현
    
    * :lipstick: snackbar 컴포넌트 storybook파일 생성
    
    * :lipstick: snackbar width 추가

---
+++ src/components/snackbar/snackbar.css.ts
@@ -0,0 +1,11 @@
+import { css } from '@emotion/react';
+
+export const snackbarWrapper = css({
+  width: '354px',
+  display: 'flex',
+  justifyContent: 'center',
+  borderRadius: '12px',
+  padding: '12px 0',
+  backgroundColor: '#fff',
+  boxShadow: '0px 0px 16px 2px rgba(0, 0, 0, 0.12)',
+});
---
+++ src/components/snackbar/snackbar.stories.ts
@@ -0,0 +1,27 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import Snackbar from './snackbar';
+
+const meta: Meta<typeof Snackbar> = {
+  title: 'Common/Snackbar',
+  component: Snackbar,
+  tags: ['autodocs'],
+  parameters: {
+    layout: 'centered',
+  },
+} satisfies Meta<typeof Snackbar>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const HomeSnackbar: Story = {
+  args: {
+    variants: 'home',
+  },
+};
+
+export const LetterSnackbar: Story = {
+  args: {
+    variants: 'letter',
+  },
+};
