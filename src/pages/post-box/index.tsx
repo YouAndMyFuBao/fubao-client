394f0a27ec7ab80c06fa60853870912962f08e3e
anonymousrecords <a.Records2022@gmail.com>
2023-12-27 23:14:52 +0900

    :sparkles: 무한 스크롤 구현

--- src/pages/post-box/index.tsx
+++ src/pages/post-box/index.tsx
@@ -3,25 +3,35 @@ import HomeHeader from '../home/_components/home-header';
 import * as Style from './post-box.css';
 import { useRouter } from 'next/navigation';
 import Post from './_components/post';
-import { useQuery } from '@tanstack/react-query';
+import { useInfiniteQuery } from '@tanstack/react-query';
 import { getPostBox } from '@/apis/getPostBox';
+// import { useRef } from 'react';
+import useIntersect from '@/hooks/useIntersect';
 
 const PostBox = () => {
   const router = useRouter();
+  // const observRef = useRef(null);
   const {
     data: postBoxData,
-    isLoading,
-    error,
-  } = useQuery({
+    isFetching,
+    fetchNextPage,
+    hasNextPage,
+  } = useInfiniteQuery({
     queryKey: ['postBoxData'],
-    queryFn: () => getPostBox(1, 5),
+    initialPageParam: 0,
+    queryFn: getPostBox,
+    getNextPageParam: (lastPage, allPages) => {
+      return lastPage.last ? undefined : allPages.length + 1;
+    },
   });
 
-  if (isLoading) return '로딩중입니다';
-
-  if (error) return '에러발생에러발생: ' + error;
-
-  if (!postBoxData) return '데이터가 없어요';
+  const targetRef = useIntersect(
+    (entry, observer) => {
+      if (entry.isIntersecting) observer.unobserve(entry.target);
+      if (hasNextPage && !isFetching) fetchNextPage();
+    },
+    { threshold: 0.6 },
+  );
 
   return (
     <>
@@ -30,23 +40,25 @@ const PostBox = () => {
         <div>
           <main css={Style.base}>
             <div css={Style.postContainer}>
-              <Post imgSrc="/assets/img/fubao.jpeg" />
-              <Post imgSrc="/assets/img/fubao.jpeg" />
-              <Post imgSrc="/assets/img/fubao.jpeg" />
-              <Post imgSrc="/assets/img/fubao.jpeg" />
-              <Post imgSrc="/assets/img/fubao.jpeg" />
-              <Post imgSrc="/assets/img/fubao.jpeg" />
-              <Post imgSrc="/assets/img/fubao.jpeg" />
-              <Post imgSrc="/assets/img/fubao.jpeg" />
+              {postBoxData?.pages.map((page) =>
+                page.content.map((post) => <Post key={post.postId} post={post} />),
+              )}
+              <div ref={targetRef} />
             </div>
           </main>
-          <div css={Style.bodyContent}>
-            <Button
-              variants="primary"
-              size="large"
-              style={{ position: 'fixed', bottom: '41px', zIndex: '10', display: 'flex' }}
-              onClick={() => router.push('/letter')}
-            >
+          <div
+            style={{
+              position: 'fixed',
+              bottom: '41px',
+              zIndex: '10',
+              display: 'flex',
+              justifyContent: 'center',
+              width: '100%',
+              minWidth: '390px',
+              maxWidth: '450px',
+            }}
+          >
+            <Button variants="primary" size="large" onClick={() => router.push('/letter')}>
               {`편지 쓰러가기`}
             </Button>
           </div>
