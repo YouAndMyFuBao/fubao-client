52a66bfc0d02db6cabd7eee125c705cec042234a
anonymousrecords <a.Records2022@gmail.com>
2023-12-27 23:14:14 +0900

    :recycle: InfinitePostData 추가

--- src/data/type.ts
+++ src/data/type.ts
@@ -5,8 +5,38 @@ export interface APIResponse<T> {
   data: T;
 }
 
-export type PostBoxData = {
+export type PostData = {
+  postId: number;
   imageUrl: string;
   content: string;
-  time: string;
+  date: string;
+};
+
+export type InfinitePostData = {
+  content: PostData[];
+  pageable: {
+    pageNumber: number;
+    pageSize: number;
+    sort: {
+      empty: boolean;
+      sorted: boolean;
+      unsorted: boolean;
+    };
+    offset: number;
+    paged: boolean;
+    unpaged: boolean;
+  };
+  totalPages: number;
+  totalElements: number;
+  last: boolean;
+  size: number;
+  number: number;
+  sort: {
+    empty: boolean;
+    sorted: boolean;
+    unsorted: boolean;
+  };
+  numberOfElements: number;
+  first: boolean;
+  empty: boolean;
 };
