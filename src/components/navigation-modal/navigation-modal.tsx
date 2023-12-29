0be5e1805d5a0eb350dace739517acdf0cb3fc93
anonymousrecords <a.Records2022@gmail.com>
2023-12-29 21:27:15 +0900

    :sparkles: 로그아웃, 회원탈퇴 기능 구현

--- src/components/navigation-modal/navigation-modal.tsx
+++ src/components/navigation-modal/navigation-modal.tsx
@@ -1,7 +1,12 @@
 import { AnimatePresence, motion } from 'framer-motion';
-import * as navigationModal from './navigation-modal.css';
+import * as Style from './navigation-modal.css';
 import Image from 'next/image';
 import Link from 'next/link';
+import { getCookie } from 'cookies-next';
+import { useState } from 'react';
+import { postLogout } from '@/apis/postLogout';
+import { deleteDeactivation } from '@/apis/deleteDeactivation';
+import { useRouter } from 'next/router';
 
 interface NavigationModalProps {
   isOpen: boolean;
@@ -38,6 +43,95 @@ const navigationList: TabType[] = [
 ];
 
 export default function NavigationModal({ isOpen, onClose }: NavigationModalProps) {
+  const router = useRouter();
+  const accessToken = getCookie('accessToken');
+
+  const [modal, setModal] = useState<{ isOpen: boolean; type: 'login' | 'logout' | 'delete' }>({
+    isOpen: false,
+    type: 'login',
+  });
+
+  const kakaoLogin = () => {
+    window.location.href = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env
+      .NEXT_PUBLIC_KAKAO_REST_API_KEY!}&redirect_uri=${
+      process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI
+    }&response_type=code`;
+  };
+
+  function SwitchModal({ type }: { type: 'login' | 'logout' | 'delete' }) {
+    switch (type) {
+      case 'login':
+        return (
+          <>
+            <button css={Style.example} onClick={() => kakaoLogin()}>
+              login
+            </button>
+            <button
+              css={Style.exampleSecond}
+              onClick={() => {
+                setModal({
+                  isOpen: false,
+                  type: 'login',
+                });
+              }}
+            >
+              닫기
+            </button>
+          </>
+        );
+      case 'logout':
+        return (
+          <>
+            <button
+              css={Style.example}
+              onClick={async () => {
+                await postLogout();
+                router.reload();
+              }}
+            >
+              logout
+            </button>
+            <button
+              css={Style.exampleSecond}
+              onClick={() => {
+                setModal({
+                  isOpen: false,
+                  type: 'logout',
+                });
+              }}
+            >
+              닫기
+            </button>
+          </>
+        );
+      case 'delete':
+        return (
+          <>
+            <button
+              css={Style.example}
+              onClick={async () => {
+                await deleteDeactivation();
+                router.reload();
+              }}
+            >
+              delete
+            </button>
+            <button
+              css={Style.exampleSecond}
+              onClick={() => {
+                setModal({
+                  isOpen: false,
+                  type: 'delete',
+                });
+              }}
+            >
+              닫기
+            </button>
+          </>
+        );
+    }
+  }
+
   return (
     <AnimatePresence>
       {isOpen && (
@@ -47,9 +141,9 @@ export default function NavigationModal({ isOpen, onClose }: NavigationModalProp
           exit={{ opacity: 0, y: 1000 }}
           transition={{ duration: 0.5, ease: 'easeInOut' }}
         >
-          <div css={navigationModal.base}>
-            <header css={navigationModal.modalHeader}>
-              <button css={navigationModal.modalCloseButton} onClick={onClose}>
+          <div css={Style.base}>
+            <header css={Style.modalHeader}>
+              <button css={Style.modalCloseButton} onClick={onClose}>
                 <Image
                   alt="exit-button"
                   src="/assets/svgs/IconArrowBack.svg"
@@ -63,9 +157,8 @@ export default function NavigationModal({ isOpen, onClose }: NavigationModalProp
                 <motion.div
                   initial={{ opacity: 0, y: 0 }}
                   animate={{ opacity: 1, y: 0 }}
-                  // exit={{ opacity: 0, y: 1000 }}
                   transition={{ duration: 1.3, ease: 'easeInOut' }}
-                  css={navigationModal.modalButton}
+                  css={Style.modalButton}
                 >
                   <p>{tab.title}</p>
                   <Image
@@ -77,7 +170,60 @@ export default function NavigationModal({ isOpen, onClose }: NavigationModalProp
                 </motion.div>
               </Link>
             ))}
+            <div>
+              {accessToken ? (
+                <motion.div
+                  initial={{ opacity: 0, y: 0 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  transition={{ duration: 1.3, ease: 'easeInOut' }}
+                  css={Style.memberManagement}
+                >
+                  <button
+                    css={Style.memberManagementButton}
+                    onClick={() => {
+                      setModal({
+                        isOpen: true,
+                        type: 'logout',
+                      });
+                    }}
+                  >
+                    로그아웃
+                  </button>
+                  <button
+                    css={Style.memberManagementButton}
+                    onClick={() => {
+                      setModal({
+                        isOpen: true,
+                        type: 'delete',
+                      });
+                    }}
+                  >
+                    회원탈퇴
+                  </button>
+                </motion.div>
+              ) : (
+                <motion.div
+                  initial={{ opacity: 0, y: 0 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  transition={{ duration: 1.3, ease: 'easeInOut' }}
+                  css={Style.memberManagement}
+                >
+                  <button
+                    css={Style.memberManagementButton}
+                    onClick={() => {
+                      setModal({
+                        isOpen: true,
+                        type: 'login',
+                      });
+                    }}
+                  >
+                    로그인
+                  </button>
+                </motion.div>
+              )}
+            </div>
           </div>
+          {modal.isOpen && <SwitchModal type={modal.type} />}
         </motion.div>
       )}
     </AnimatePresence>
