10da54ab0ce6a228c3fa4f75e4b91dcca73fadb2
anonymousrecords <a.Records2022@gmail.com>
2023-12-29 16:52:05 +0900

    :zap: 홈 화면 구현

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,10 +1,46 @@
+import { useState } from 'react';
 import HomeHeader from './_components/home-header';
+import HomeMessage from './_components/home-message';
+import ScratchableImage from './_components/scratchable-image';
+import HomeMessageBottom from './_components/home-message-bottom';
+import { useRouter } from 'next/router';
+import { getCookie } from 'cookies-next';
+import MyLetterButton from './_components/my-letter-button';
 
 const Home = () => {
+  const [scratch, setScratch] = useState(false);
+  const router = useRouter();
+  const cookie = getCookie('accessToken');
+
   return (
-    <>
+    <div
+      style={{
+        marginTop: '39px',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
       <HomeHeader />
-    </>
+      <HomeMessage scratchableEnd={scratch} />
+      <ScratchableImage
+        onScratchEnd={() => {
+          setScratch(true);
+        }}
+      />
+      {!scratch && <HomeMessageBottom />}
+      {scratch && cookie && <MyLetterButton onClick={() => router.push('/my-letter')} />}
+      {scratch && (
+        <>
+          <button>링크 복사하기</button>
+          <div>
+            <button onClick={() => router.reload()}>다시 하트주기</button>
+            <button onClick={() => router.push('/letter')}>편지 쓰러가기</button>
+          </div>
+        </>
+      )}
+    </div>
   );
 };
 
