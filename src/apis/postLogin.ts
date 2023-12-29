beab808d8031d5a25ff17d946029ba9886b0af58
anonymousrecords <a.Records2022@gmail.com>
2023-12-29 16:49:51 +0900

    :recycle: data -> data.data로 수정

--- src/apis/postLogin.ts
+++ src/apis/postLogin.ts
@@ -2,10 +2,12 @@ import { baseAxios } from './baseAxios';
 
 export const postLogin = async (authorizationCode: string) => {
   const data = await baseAxios.post<{
-    accessToken: string;
-    refreshToken: string;
+    data: {
+      accessToken: string;
+      refreshToken: string;
+    };
   }>('api/auth/kakao', {
     authorizationCode: authorizationCode,
   });
-  return data;
+  return data.data;
 };
