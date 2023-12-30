023176a53c33b153b52fbc600f10baf3195e692c
anonymousrecords <a.Records2022@gmail.com>
2023-12-30 16:27:24 +0900

    :zap: 디자인 변경에 따른 button 컴포넌트 수정

--- src/components/button/button.css.ts
+++ src/components/button/button.css.ts
@@ -11,39 +11,19 @@ export const base = css({
   fontSize: '17px',
   fontWeight: '600',
   borderRadius: '16px',
+  width: '354px',
+  height: '56px',
 });
 
 export const variantsCss: Record<ButtonProps['variants'], SerializedStyles> = {
-  primary: css({
+  'one-button-enable-none': css({
     backgroundColor: '#1E212B',
     color: 'white',
     boxShadow: '0px 0px 16px 2px rgba(0, 0, 0, 0.02)',
     '&:disabled': {
-      backgroundColor: '#909190',
+      backgroundColor: '#A7C2B1',
+      color: 'white',
       cursor: 'default',
     },
   }),
-  secondary: css({
-    backgroundColor: 'white',
-    color: '#1E212B',
-    boxShadow: '0px 0px 16px 2px rgba(0, 0, 0, 0.12)',
-  }),
-  'green-primary': css({
-    backgroundColor: '#009436',
-    color: 'white',
-  }),
-  'green-secondary': css({
-    backgroundColor: '#A7C2B1',
-    color: '#1E212B',
-  }),
-  login: css({
-    backgroundColor: '#FEE500',
-    color: '#000',
-  }),
-};
-
-export const sizeCss: Record<NonNullable<ButtonProps['size']>, SerializedStyles> = {
-  small: css({ width: '100px', height: '56px' }),
-  medium: css({ width: '172px', height: '56px' }),
-  large: css({ width: '354px', height: '56px' }),
 };
