6bf4b38c4ca40d1a0b41d7477b6fee46c0da7141
anonymousrecords <a.Records2022@gmail.com>
2024-01-01 18:37:22 +0900

    :sparkles: 하단 하트 개수 메세지 노출 기능 구현

--- src/pages/home/_components/home-message-bottom.tsx
+++ src/pages/home/_components/home-message-bottom.tsx
@@ -1,6 +1,11 @@
 import * as Style from './home-message-bottom.css';
+import { useQuery } from '@tanstack/react-query';
+import { getFubaoLove } from '../../../apis/getFubaoLove';
 
 export default function HomeMessageBottom() {
-  const visitors = 100;
-  return <p css={Style.base}>지금까지 바오에게 전달된 사랑은 총 {visitors}개</p>;
+  const { data: loveData } = useQuery({
+    queryKey: ['fubaoLove'],
+    queryFn: getFubaoLove,
+  });
+  return <p css={Style.base}>지금까지 바오에게 전달된 사랑은 총 {loveData?.data.love}개</p>;
 }
