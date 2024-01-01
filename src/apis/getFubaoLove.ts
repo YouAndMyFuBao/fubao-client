8e9e37c36ef85c07fd6d01f10cb8abc285d74eb8
anonymousrecords <a.Records2022@gmail.com>
2024-01-01 18:40:51 +0900

    :fire: 불필요한 log 코드 삭제

--- src/apis/getFubaoLove.ts
+++ src/apis/getFubaoLove.ts
@@ -4,6 +4,5 @@ import { APIResponse, ReceivedLove } from '@/data/type';
 
 export const getFubaoLove = async () => {
   const data = await baseAxios.get<APIResponse<ReceivedLove>>(`/api/posts/fubao/love`);
-  console.log('푸바오 받은 사랑', data.data);
   return data.data;
 };
--- src/apis/postFubaoLove.ts
+++ src/apis/postFubaoLove.ts
@@ -4,6 +4,6 @@ import { baseAxios } from './baseAxios';
 
 export const postFubaoLove = async () => {
   const data = await baseAxios.post<APIResponse<DeliverLove>>('/api/posts/fubao/love');
-  console.log('푸바오에게 사랑 보내기', data);
-  return data;
+  console.log('푸바오에게 사랑 보내기', data.data);
+  return data.data;
 };
