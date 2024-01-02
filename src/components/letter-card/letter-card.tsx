ade34dbc7a2af40483f8f67a28950bbba79fb999
eunchokang <wkdb0425@gmail.com>
2024-01-02 17:11:58 +0900

    :sparkles: 서버에 저장된 편지 수정 때 이미지 수정가능하도록 변경

--- src/components/letter-card/letter-card.tsx
+++ src/components/letter-card/letter-card.tsx
@@ -23,23 +23,31 @@ type LetterCardProps = TextCountCardProps | DateCardProps;
 export default function LetterCard(props: LetterCardProps) {
   const { letterImage, setLetterImage, letterText, setLetterText } = useLetterContext();
   const [letterTextLength, setLetterTextLength] = useState<number>(0);
+  const [imageInCard, setImageInCard] = useState<string | undefined>(undefined);
 
   useEffect(() => {
     if (props.apiText) {
       setLetterText(props.apiText);
       setLetterTextLength(props.apiText.length);
     }
+    if (props.apiImage) {
+      setImageInCard(props.apiImage);
+    }
   }, []);
+
+  useEffect(() => {}, [imageInCard]);
 
   const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.files) {
       const newImage = e.target.files[0];
-      setLetterImage(newImage);
+      const storedImageUrl = URL.createObjectURL(newImage);
+      setImageInCard(storedImageUrl);
     }
   };
 
   const handleDeleteButtonClick = () => {
     setLetterImage(undefined);
+    setImageInCard(undefined);
   };
 
   const handleInputChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
@@ -50,34 +58,9 @@ export default function LetterCard(props: LetterCardProps) {
     setLetterTextLength(e.target.value.length);
   };
 
-  const ConditionalRenderTextCount = () => {
-    if (letterImage) {
-      console.log('letterImage', letterImage);
-      return (
-        <div css={Style.imageUploaded.wrapper}>
-          <img
-            src={URL.createObjectURL(letterImage)}
-            css={Style.imageUploaded.image}
-            alt="postImageSelected"
-          />
-          <button type="button" onClick={handleDeleteButtonClick}>
-            <IconDelete css={Style.imageUploaded.deleteImageIcon} />
-          </button>
-        </div>
-      );
-    }
-    if (props.apiImage) {
-      console.log('props.apiImage', props.apiImage);
-      return (
-        <div css={Style.imageUploaded}>
-          <div css={Style.imageUploaded.wrapper}>
-            <img src={props.apiImage} css={Style.imageUploaded.image} alt="postImageSelected" />
-          </div>
-        </div>
-      );
-    }
-    if (!letterImage && !props.apiImage) {
-      console.log('letterImage', letterImage, 'props.apiImage', props.apiImage);
+  const ConditionalRenderImage = () => {
+    if (!imageInCard) {
+      console.log('imageInCard', imageInCard);
       return (
         <div css={Style.noneImageUploaded.wrapper}>
           <label htmlFor="file" css={Style.noneImageUploaded.label}>
@@ -93,6 +76,17 @@ export default function LetterCard(props: LetterCardProps) {
           />
         </div>
       );
+    } else {
+      console.log('letterImage', letterImage);
+      console.log('imageInCard', imageInCard);
+      return (
+        <div css={Style.imageUploaded.wrapper}>
+          <img src={imageInCard} css={Style.imageUploaded.image} alt="postImageSelected" />
+          <button type="button" onClick={handleDeleteButtonClick}>
+            <IconDelete css={Style.imageUploaded.deleteImageIcon} />
+          </button>
+        </div>
+      );
     }
   };
 
@@ -100,7 +94,7 @@ export default function LetterCard(props: LetterCardProps) {
     <>
       {props.variant === 'textCount' && (
         <div css={Style.letterWrapper} className={letterFont.className}>
-          {ConditionalRenderTextCount()}
+          {ConditionalRenderImage()}
           <div css={Style.mainText.wrapper}>
             <textarea
               value={letterText}
